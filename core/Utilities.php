cfc21c95774af7dddbbad8573b3c4c33c65d6aaf
Jabari Hunt <jabari@jabari.net>
2018-11-13 00:10:19 -0600

    Added Utilities::getClientIP() method.

--- core/Utilities.php
+++ core/Utilities.php
@@ -172,6 +172,22 @@
 
                         return $emailIsValid;
                 }
+
+            /********************************************************************************
+             * GET CLIENT IP METHOD
+             * @return string
+             ********************************************************************************/
+
+                public static function getClientIP()
+                {
+                    $ip = '';
+
+                    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {$ip = $_SERVER['HTTP_CLIENT_IP'];}
+                    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];}
+                    else {$ip = $_SERVER['REMOTE_ADDR'];}
+
+                    return $ip;
+                }
         }
 
 ?>
