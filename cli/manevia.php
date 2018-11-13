4ee9e53f260d52203fe4a6001bf2ac5c6359fe1b
Jabari Hunt <jabari@jabari.net>
2018-11-13 01:52:31 -0600

    Altered some manevia cli command names.  Database backups now store in backups/database directory.

--- cli/manevia.php
+++ cli/manevia.php
@@ -3,7 +3,7 @@
 
     // SET VALID COMMANDS CONSTANT | IMPORT .env VALUES
 
-        const VALID_COMMANDS = ['backup_db', 'deploy', 'help', 'models'];
+        const VALID_COMMANDS = ['backup-db', 'deploy', 'help', 'build-models'];
 
         require('/var/www/html/vendor/autoload.php');
         $dotenv = new \Dotenv\Dotenv('/var/www/html');
@@ -17,10 +17,10 @@
 
                 switch ($argv[1])
                 {
-                    case 'backup_db': shell_exec("mysqldump --user='{$_ENV['DATABASE_USER']}' --password='{$_ENV['DATABASE_PASSWORD']}' {$_ENV['DATABASE_NAME']} > {$_ENV['DEVELOPMENT_WEB_ROOT']}/backup/backup_db-" . time() . '.sql'); break;
-                    case 'deploy'   : break; //echo shell_exec('/var/www/html/cli/build.sh'); break;
-                    case 'models'   : require('/var/www/html/cli/model_builder_docs/BaseModelBuilder.php'); break;
-                    case   'help'   : displayCommands(); break;
+                    case 'backup-db'   : shell_exec("mysqldump --user='{$_ENV['DATABASE_USER']}' --password='{$_ENV['DATABASE_PASSWORD']}' {$_ENV['DATABASE_NAME']} > {$_ENV['WEB_ROOT']}/backup/database/backup_db-" . time() . '.sql'); break;
+                    case 'deploy'      : break; //echo shell_exec('/var/www/html/cli/build.sh'); break;
+                    case 'build-models': require('/var/www/html/cli/model_builder_docs/BaseModelBuilder.php'); break;
+                    case 'help'        : displayCommands(); break;
                 }
         }
         else {displayCommands();}
@@ -33,21 +33,21 @@
 
             // COMMAND HEADER
             echo "\r\n";
-            echo sprintf($format, "         --------------------------");
-            echo sprintf($format, "         - Valid manevia commands -");
-            echo sprintf($format, "         --------------------------\r\n");
+            echo sprintf($format, "      --------------------------");
+            echo sprintf($format, "      - Valid manevia commands -");
+            echo sprintf($format, "      --------------------------\r\n");
 
             $format = "%20s   %s\r\n";
 
             // backup_db
-            echo sprintf($format, 'backup_db:', "Does a MySQL dump of the database (requires DB credentials).");
+            echo sprintf($format, 'backup-db:', "Does a MySQL dump of the database (requires DB credentials).");
             echo sprintf($format, '', "Backups are located in the /backup directory.\r\n");
 
             // deploy
             echo sprintf($format, 'deploy:', "Experimental, explore with your own risk!\r\n");
 
             // models
-            echo sprintf($format, 'models:', "Updates base models from database.");
+            echo sprintf($format, 'build-models:', "Updates base models from database.");
             echo sprintf($format, '', "NOTE: This will not overwrite changes to you custom methods!\r\n");
 
             // help
