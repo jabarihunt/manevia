bc8d41c2ed14c611f9b05772cad84d84112c7995
Jabari Hunt <jabari@jabari.net>
2020-04-04 23:32:23 -0400

    Egyptianized and 7ized DB.php.

--- app/core/DB.php
+++ app/core/DB.php
@@ -1,6 +1,8 @@
 <?php namespace Manevia;
 
-    use \mysqli;
+    use mysqli;
+    use mysqli_result;
+    use mysqli_stmt;
 
     /********************************************************************************
      * MYSQLI DATABASE & PHP SESSION HANDLER
@@ -38,8 +40,8 @@
      *
      ********************************************************************************/
 
-        final class DB
-        {
+        final class DB {
+
             /********************************************************************************
              * CONNECTION AND SESSION VARIABLES
              * @var string $host Database Server
@@ -96,21 +98,22 @@
 
             /********************************************************************************
              * SINGLETON INSTANCE METHOD
-             * @return object
+             * @return mysqli
              ********************************************************************************/
 
-                final private static function get()
-                {
+                final private static function get(): mysqli {
+
                     if(self::$instance === NULL) {self::$instance = new DB();}
                     return self::$db;
+
                 }
 
             /********************************************************************************
              * CONSTRUCTOR
              ********************************************************************************/
 
-                final private function __construct()
-                {
+                final private function __construct() {
+
                     // SET DATABASE AND SESSION CLASS VARIABLES
 
                         self::$host           = getenv('DATABASE_HOST');
@@ -120,28 +123,31 @@
                         self::$useDBSessions  = (boolean) getenv('DATABASE_SESSION_STORE_IN_DB');
                         self::$sessionExpires = (integer) getenv('DATABASE_SESSION_EXPIRES');
 
-                        if
-                        (
+                        if (
                             self::$host === 'localhost' &&
                             !empty(getenv('DATABASE_SOCKET')) &&
                             getenv('DATABASE_SOCKET') !== 'NULL'
-                        )
-                        {self::$socket = getenv('DATABASE_SOCKET');}
-                        else {self::$socket = NULL;}
+                        ) {
+                            self::$socket = getenv('DATABASE_SOCKET');
+                        } else {
+                            self::$socket = NULL;
+                        }
 
                     // CONNECT TO DATABASE | CHECK CONNECTION
 
                         self::$db = self::$socket === NULL ? new mysqli(self::$host, self::$user, self::$password, self::$database) : new mysqli(NULL, self::$user, self::$password, self::$database, NULL, self::$socket);
-                        if (self::$db->connect_error === TRUE) {die ('<b style="color: #F00;">COULD NOT CONNECT TO THE DATABASE SERVER</b>');}
+
+                        if (self::$db->connect_error === TRUE) {
+                            die ('<b style="color: #F00;">COULD NOT CONNECT TO THE DATABASE SERVER</b>');
+                        }
 
                     // START SESSION IF REQUESTED
 
-                        if (self::$useDBSessions)
-                        {
+                        if (self::$useDBSessions) {
+
                             // RUN SET SESSION HANDLER METHOD | START SESSION
 
-                                session_set_save_handler
-                                (
+                                session_set_save_handler (
                                     [$this, 'openSession'],
                                     [$this, 'closeSession'],
                                     [$this, 'readSession'],
@@ -151,22 +157,29 @@
                                 );
 
                                 session_start();
+
                         }
+
                 }
 
             /********************************************************************************
              * DESTRUCTOR
              ********************************************************************************/
 
-                final public function __destruct()
-                {
+                final public function __destruct() {
+
                     // CLOSE SESSION | CLOSE DATABASE
 
-                        if (is_object(self::$db))
-                        {
-                            if (self::$useDBSessions) {session_write_close();}
+                        if (self::$db instanceof mysqli) {
+
+                            if (self::$useDBSessions) {
+                                session_write_close();
+                            }
+
                             self::$db->close();
+
                         }
+
                 }
 
             /********************************************************************************
@@ -178,61 +191,75 @@
                  * @param string $directory
                  ********************************************************************************/
 
-                    final public static function backup($directory)
-                    {
+                    final public static function backup(string $directory) {
+
                         $user     = self::$user;
                         $password = self::$password;
                         $database = self::$database;
                         $location = rtrim($directory, '/') . "/{$database}-" . date('Ymd') . '_' . time() . '.sql';
 
                         exec("mysqldump --user='{$user}' --password='{$password}' --single-transaction --routines --triggers {$database} > {$location}");
+
                     }
 
                 /********************************************************************************
                  * INITIALIZE METHOD
-                 * @param boolean $useSessions
                  ********************************************************************************/
 
-                    final public static function initialize() {self::get();}
+                    final public static function initialize() {
+                        self::get();
+                    }
 
                 /********************************************************************************
                  * PREPARE METHOD
                  * @param string $query
-                 * @returns \mysqli_stmt
+                 * @return mysqli_stmt
                  ********************************************************************************/
 
-                    final public static function prepare($query) {return self::get()->prepare($query);}
+                    final public static function prepare(string $query): mysqli_stmt {
+                        return self::get()->prepare($query);
+                    }
 
                 /********************************************************************************
                  * QUERY METHOD
                  * @param string $query
-                 * @returns \mysqli_result
+                 * @return mysqli_result
                  ********************************************************************************/
 
-                    final public static function query($query) {return self::get()->query($query);}
+                    final public static function query(string $query): mysqli_result {
+                        return self::get()->query($query);
+                    }
 
             /********************************************************************************
              * SANITIZE METHOD
              * Used to sanitize individual field values for database insertion.
              * @param mixed $value The value to be sanitized.
              * @param string $dataType The DB datatype of the passed value
-             * @returns mixed
+             * @return mixed
              ********************************************************************************/
 
-                final public static function sanitize($value, $dataType = NULL)
-                {
+                final public static function sanitize($value, string $dataType = NULL) {
+
                     // SANITIZE BASED ON FILTER TYPE | RETURN VALUE
 
-                        if ($dataType !== NULL)
-                        {
-                            if ($dataType === self::DATA_TYPE_INTEGER) {$value = filter_var($value, FILTER_SANITIZE_NUMBER_INT);}
-                            else if ($dataType === self::DATA_TYPE_TEXT) {$value = filter_var($value, FILTER_SANITIZE_STRING);}
-                            else if ($dataType === self::DATA_TYPE_REAL) {$value = filter_var($value, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);}
-                            else {$value = filter_var($value, FILTER_SANITIZE_STRING);}
+                        if ($dataType !== NULL) {
+
+                            if ($dataType === self::DATA_TYPE_INTEGER) {
+                                $value = filter_var($value, FILTER_SANITIZE_NUMBER_INT);
+                            } else if ($dataType === self::DATA_TYPE_TEXT) {
+                                $value = filter_var($value, FILTER_SANITIZE_STRING);
+                            } else if ($dataType === self::DATA_TYPE_REAL) {
+                                $value = filter_var($value, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+                            } else {
+                                $value = filter_var($value, FILTER_SANITIZE_STRING);
+                            }
+
+                        } else {
+                            $value = filter_var($value, FILTER_SANITIZE_STRING);
                         }
-                        else {$value = filter_var($value, FILTER_SANITIZE_STRING);}
 
                         return trim($value);
+
                 }
 
             /********************************************************************************
@@ -240,12 +267,12 @@
              * NOTE: BOTH close() AND destroy($session_id) MUST BE PUBLIC IF session_destroy() IS USED IN SCRIPTS!!!
              ********************************************************************************/
 
-                final public function openSession() {return TRUE;}
+                final public function openSession(): bool {return TRUE;}
 
-                final public function closeSession() {return TRUE;}
+                final public function closeSession(): bool {return TRUE;}
 
-                final public function readSession($sessionId)
-                {
+                final public function readSession($sessionId) {
+
                     // SET CURRENT AND EXPIRATION TIME
 
                         $now     = time();
@@ -257,43 +284,40 @@
 
                         $result = self::$db->query("SELECT data FROM sessions WHERE id = '{$sessionId}' AND expires > {$now}");
 
-                        if ($result->num_rows == 1)
-                        {
+                        if ($result->num_rows == 1) {
+
                             // GET SESSION DATA | SET NEW EXPIRATION
 
                                 $session = $result->fetch_assoc();
                                 $data    = $session['data'];
 
                                 self::$db->query("UPDATE sessions SET expires = {$expires} WHERE id = '{$sessionId}'");
+
+                        } else {
+                            $data = '';
                         }
-                        else {$data = '';}
 
                         return $data;
+
                 }
 
-                final public function writeSession($sessionId, $sessionData)
-                {
+                final public function writeSession($sessionId, $sessionData) {
+
                     // SET INITIAL VARIABLES | REPLACE/INSERT SESSION DATA IN DATABASE -> RETURN RESULT
 
                         $expires        = time() + (self::$sessionExpires * 60);
                         $dbSessionData  = self::$db->real_escape_string($sessionData);
 
                         return self::$db->query("INSERT INTO sessions (id, data, expires) VALUES ('{$sessionId}', '{$dbSessionData}', '{$expires}') ON DUPLICATE KEY UPDATE data = '{$dbSessionData}', expires = {$expires}");
+
                 }
 
-                final public function destroySession($sessionId)
-                {
-                    // DELETE SESSION FROM DATABASE AND RETURN RESULT
-
-                        return self::$db->query("DELETE FROM sessions WHERE id = {$sessionId}");
+                final public function destroySession($sessionId) {
+                    return self::$db->query("DELETE FROM sessions WHERE id = {$sessionId}");
                 }
 
-                final public function gcSession()
-                {
-                    // GET CURRENT TIMESTAMP | DELETE OLD SESSIONS -> RETURN RESULT
-
-                        $now = time();
-                        return self::$db->query("DELETE FROM sessions WHERE expires <= {$now}");
+                final public function gcSession() {
+                    return self::$db->query('DELETE FROM sessions WHERE expires <= UNIX_TIMESTAMP()');
                 }
 
             /********************************************************************************
@@ -302,10 +326,12 @@
              * job, but can be called from anywhere.
              ********************************************************************************/
 
-                final public static function startSessionGarbageCollection()
-                {
+                final public static function startSessionGarbageCollection() {
+
                     self::get();
                     self::$instance->gcSession();
+
                 }
+
         }
 ?>
