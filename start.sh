9ac32734bf34a60428591a14ae258c7226b49867
Jabari Hunt <jabari.hunt@performics.com>
2021-02-24 01:55:52 -0500

    Minor code cleanup.

--- start.sh
+++ start.sh
@@ -22,10 +22,10 @@ fi
 if test -d app/vendor && test -f .env; then
   echo -e "MANEVIA: Starting Docker...\n"
   source ".env"
-  docker build . -t ${DOCKER_TAG_VERSION} -t ${DOCKER_TAG_LATEST}
+  docker build . -t "${DOCKER_TAG_VERSION}" -t "${DOCKER_TAG_LATEST}"
   docker run -d \
-  -p 8080:${DOCKER_PORT} \
-    -e PORT=${DOCKER_PORT} \
+  -p 8080:"${DOCKER_PORT}" \
+    -e PORT="${DOCKER_PORT}" \
     -e ENVIRONMENT="${ENVIRONMENT}" \
     -e DEFAULT_API_VERSION="${DEFAULT_API_VERSION}" \
     -e DEFAULT_AUTH_REQUIRED="${DEFAULT_AUTH_REQUIRED}" \
@@ -33,11 +33,8 @@ if test -d app/vendor && test -f .env; then
     -e DATABASE_NAME="${DATABASE_NAME}" \
     -e DATABASE_USER="${DATABASE_USER}" \
     -e DATABASE_PASSWORD="${DATABASE_PASSWORD}" \
-    -e DATABASE_SOCKET=${DATABASE_SOCKET} \
-    -e SESSION_ENABLED=${SESSION_ENABLED} \
-    -e SESSION_SAVE_HANDLER="${SESSION_SAVE_HANDLER}" \
-    -e SESSION_SAVE_PATH="${SESSION_SAVE_PATH}" \
-    ${DOCKER_APP}
+    -e DATABASE_SOCKET="${DATABASE_SOCKET}" \
+    "${DOCKER_APP}"
 else
   echo -e "MANEVIA: Missing app/vendor directory or .env file"
 fi
