4b81068d6f159cf7ded339882b99ef966e8c03e2
Jabari Hunt <jabari.hunt@performics.com>
2021-02-24 00:36:47 -0500

    Moved cli folder outside of the app folder.  Deleted the backup directory.

--- cli/model_builder_docs/BaseModelBuilder.php
+++ cli/model_builder_docs/BaseModelBuilder.php
@@ -9,7 +9,7 @@
 
         require(__DIR__ . '/../../vendor/autoload.php');
 
-        $dotenv = Dotenv::createImmutable(__DIR__ . '/../../../');
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../../manevia/');
         $dotenv->load();
 
         DB::initialize();
