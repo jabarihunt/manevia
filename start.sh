f05f185115ac3346bcc29def0dd59b66faec17bd
Jabari Hunt <jabari@jabari.net>
2021-02-28 01:02:56 -0500

    New env variables are now auto-loaded (no need to add them to the start file).  Also, all env values are double quoted in the .env.example file.

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # RUN COMPOSER IF VENDOR DIRECTORY DOESN'T EXIST
-if test ! -d app/vendor && test -x "$(which composer)"; then
+if [[ ! -d app/vendor ]] && [[ -x "$(which composer)" ]]; then
   echo -e "\nMANEVIA: Running Composer...\n"
+  # shellcheck disable=SC2164
   cd app
   composer install
   cd ../
@@ -11,30 +12,56 @@ else
 fi
 
 # CREATE .env FILE IF IT DOESN'T EXIST
-if test ! -f .env; then
+if [[ ! -f .env ]]; then
   echo -e "\nMANEVIA: Creating .env file..."
   cp .env.example .env
 else
   echo -e "MANEVIA: .env file exists"
 fi
 
-# LOAD ENVIRONMENT FILE AND START DOCKER
-if test -d app/vendor && test -f .env; then
+# BUILD & RUN DOCKER
+if [[ -d app/vendor ]] && [[ -f .env ]]; then
   echo -e "MANEVIA: Starting Docker...\n"
-  source ".env"
-  docker build . -t "${DOCKER_TAG_VERSION}" -t "${DOCKER_TAG_LATEST}"
-  docker run -d \
-  -p 8080:"${DOCKER_PORT}" \
-    -e PORT="${DOCKER_PORT}" \
-    -e ENVIRONMENT="${ENVIRONMENT}" \
-    -e DEFAULT_API_VERSION="${DEFAULT_API_VERSION}" \
-    -e DEFAULT_AUTH_REQUIRED="${DEFAULT_AUTH_REQUIRED}" \
-    -e DATABASE_HOST="${DATABASE_HOST}" \
-    -e DATABASE_NAME="${DATABASE_NAME}" \
-    -e DATABASE_USER="${DATABASE_USER}" \
-    -e DATABASE_PASSWORD="${DATABASE_PASSWORD}" \
-    -e DATABASE_SOCKET="${DATABASE_SOCKET}" \
-    "${DOCKER_APP}"
+
+  # CREATE BUILD & RUN STRINGS
+  dockerBuildString="docker build ."
+  dockerRunString="docker run -d"
+
+  while IFS= read -r line || [ -n "$line" ]; do
+
+    firstChar=${line:0:1}
+
+    if ! [[ $firstChar = "#" ]]  && [[ $line = *"="* ]]; then
+
+      # GET FIELD AND VALUE
+      field="$(cut -d'=' -f1 <<< "$line")"
+      value="$(cut -d'=' -f2 <<< "$line")"
+      value=$(eval "echo $value")
+
+      # CREATE DOCKER RUN VALUES & STRING
+      if [[ $field = "DOCKER_TAG_VERSION" ]]; then
+        dockerBuildString="$dockerBuildString -t $value"
+      elif [[ $field = "DOCKER_TAG_LATEST" ]]; then
+        dockerBuildString="$dockerBuildString -t $value"
+      elif [[ $field = "DOCKER_APP" ]]; then
+        DOCKER_APP=$value
+      elif [[ $field = "DOCKER_PORT" ]]; then
+        dockerRunString="$dockerRunString -p 8080:\"$value\""
+        dockerRunString="$dockerRunString -e PORT=\"$value\""
+      else
+        dockerRunString="$dockerRunString -e $field=\"$value\""
+      fi
+
+    fi
+
+  done < .env
+
+  dockerRunString="$dockerRunString $DOCKER_APP"
+
+  # BUILD & RUN DOCKER
+  eval "$dockerBuildString"
+  eval "$dockerRunString"
+
 else
   echo -e "MANEVIA: Missing app/vendor directory or .env file"
 fi
